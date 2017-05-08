5e0b4b3d0e6757f966c85056484bb26487248644
Gautham Chandra <gautham.chandra@greenhouse.io>
2017-05-08 19:33:40 -0400

    Add basic tmux config

--- tmux/install.sh
+++ tmux/install.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 set -e
 
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+cd $SCRIPT_DIR
+
 echo "Installing tmux"
 brew install tmux
 
 echo "Installing tmuxinator"
 gem install tmuxinator
+
+echo "Downloading muxinator completion file for zsh"
+wget "https://raw.githubusercontent.com/tmuxinator/tmuxinator/master/completion/tmuxinator.zsh"
+mkdir -p ~/.bin && mv tmuxinator.zsh ~/.bin/tmuxinator.zsh
+
+echo "Adding a setup script used by the muxinator config to the proper place"
+mkdir -p ~/development/setup-scripts && ln -s pull-down-work-changes.sh ~/development/setup-scripts/pull-down-work-changes.sh 
+
+echo "Symlinking the tmuxinator files to home directory"
+ln -s .tmuxinator ~/.tmuxinator
---
+++ tmux/pull-down-work-changes.sh
@@ -0,0 +1,81 @@
+#!/usr/bin/env zsh
+
+YELLOW='\033[1;33m'
+RED='\033[0;31m'
+NO_COLOR='\033[0m'
+
+logInfo() {
+  echo -e "${YELLOW}$1${NO_COLOR}"
+}
+
+logError() {
+  echo -e "${RED}$1${NO_COLOR}"
+}
+
+# RVM is loaded as a binary but this setup is lost in a script.
+# it needs to be reloaded as a script
+load_rvm_as_function() {
+  [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
+}
+
+any_local_changes() {
+  git diff --quiet HEAD
+  return $? 
+}
+
+any_conflicts() {
+  git status | grep conflict
+}
+
+rebase_off_master() {
+  logInfo "Checking for any local changes"
+
+  LOCAL_CHANGES=false
+
+  if [[ "$(any_local_changes)" != "0" ]]; then
+    LOCAL_CHANGES=true
+    
+    logInfo "Stashing changes for now..."
+    git stash
+  fi
+
+  logInfo "Pulling down latest changes for master and rebasing..."
+  git checkout master && git pull --rebase && git checkout  - && git rebase master
+
+  logInfo "Installing any gems needed"
+  bundle
+
+  logInfo "Running migrations..."
+  rake db:local:migrate
+
+  if [ "$LOCAL_CHANGES" = true ]; then
+    logInfo "Restoring the local changes that were stashed..."
+    git stash pop
+  fi
+
+  FINAL_MSG="Finished with updates"
+  if any_conflicts; then
+    $FINAL_MSG="There are conflicts after updating"
+  fi
+
+  terminal-notifier -message $FINAL_MSG 
+}
+
+#=================================
+
+logInfo "Initializing RVM as a function..."
+load_rvm_as_function
+
+logInfo "Switching to work directory..."
+cd ~/development/greenhouse
+
+if [ $? != 0 ]; then
+  logError "Directory configured in script is invalid"
+  exit 1
+fi
+
+if [[ "$NO_REBASE" = true ]]; then
+  logInfo "\$NO_REBASE=true, skipping pulling changes from master"
+else
+  rebase_off_master
+fi
