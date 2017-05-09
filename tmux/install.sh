45dbcb095c286dcff281f8e2d6ad3ffd30ac7fb7
Gautham Chandra <gautham.chandra@greenhouse.io>
2017-05-09 11:53:53 -0400

    fixup tweak tmux config

--- tmux/install.sh
+++ tmux/install.sh
@@ -16,7 +16,14 @@ wget "https://raw.githubusercontent.com/tmuxinator/tmuxinator/master/completion/
 mkdir -p ~/.bin && mv tmuxinator.zsh ~/.bin/tmuxinator.zsh
 
 echo "Adding a setup script used by the muxinator config to the proper place"
-mkdir -p ~/development/setup-scripts && ln -s pull-down-work-changes.sh ~/development/setup-scripts/pull-down-work-changes.sh 
+mkdir -p ~/development/setup-scripts && ln -s `pwd`/pull-down-work-changes.sh ~/development/setup-scripts/pull-down-work-changes.sh 
 
 echo "Symlinking the tmuxinator files to home directory"
-ln -s .tmuxinator ~/.tmuxinator
+ln -s `pwd`/.tmuxinator ~/.tmuxinator
+
+echo "Downloading tmux plugin manager to ~/.tmux/plugins/tpm"
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+echo "Symlinking the tmux and powerline config over to home directory"
+ln -s `pwd`/.tmux.conf ~/.tmux.conf
+ln -s `pwd`/.tmux-status-bar.conf ~/.tmux-status-bar.conf
