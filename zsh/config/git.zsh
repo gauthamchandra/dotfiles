005a426feb35424590195f3828b75f1327a20aca
Gautham Chandra <gautham.chandra@greenhouse.io>
2017-10-16 10:20:56 -0400

    Add git util function

--- zsh/config/git.zsh
+++ zsh/config/git.zsh
@@ -9,5 +9,10 @@ alias gst='git stash'
 alias gco='git checkout'
 alias gpb='git push -u'
 
+# For finding all my branches that were merged
+glist-merged() {
+  git for-each-ref --format='%(authorname) %(refname)' --merged | grep 'Gautham Chandra refs/remotes/origin' | awk '{ gsub(/^Gautham Chandra refs\/remotes\/origin\//, ""); print }'
+}
+
 # For deleting any local branches that have already been merged on remote
 alias gprune-merged='git branch --merged | grep -v -e "master" -e "develop" | xargs -n 1 git branch -d'
