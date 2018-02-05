fa485ab6d2868074df54c7d176ed7332b25da95c
Gautham Chandra <gautham.chandra@greenhouse.io>
2018-02-05 10:18:46 -0500

    Add util methods to retry commands

--- zsh/config/util.zsh
+++ zsh/config/util.zsh
@@ -7,4 +7,31 @@ gcgrep() {
 #Zeus for preloading parts of the app for faster rspec tests
 ze () { ARGS=$@; zeus $@; ZE_EC=$?; stty sane; if [ $ZE_EC = 2 ]; then ze $ARGS; fi } 
 
+# Retries a command a specified number of times but stops early if
+# it succeeds
+retry () {
+  COMMAND=$1
+  RETRIES=$2
+  PAUSE_INTERVAL=$3
+
+  if [ -z $RETRIES ]; then
+    RETRIES=1
+  fi
+
+  for i in {1..$RETRIES}; do eval $COMMAND && break || sleep $PAUSE_INTERVAL; done
+}
+
+# Retries a command a specified number of times
+repeat_command() {
+  COMMAND=$1
+  RETRIES=$2
+  PAUSE_INTERVAL=$3
+
+  if [ -z $RETRIES ]; then
+    RETRIES=1
+  fi
+
+  for i in {1..$RETRIES}; do eval $COMMAND; sleep $PAUSE_INTERVAL; done
+}
+
 alias clear-vim-swaps='find ./ -type f -name "\.*sw[klmnop]" -delete'
