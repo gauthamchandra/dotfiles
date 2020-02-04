e75e0aa747ada00f8745154930c3e2cd37058bc2
Gautham Chandra <gchandra@dataminr.com>
2020-02-04 10:48:09 -0500

    Update zim config to account for how new versions are setup

--- zsh/install_zim.zsh
+++ zsh/install_zim.zsh
@@ -7,18 +7,7 @@ SCRIPT_DIR="${0:a:h}"
 source $SCRIPT_DIR/../util.sh
 
 function install_zim() {
-  git clone --recursive https://github.com/zimfw/zimfw.git ${ZDOTDIR:-${HOME}}/.zim
-}
-
-function configure_zim() {
-  logInfo "Prepending zim init logic to $HOME/.zshrc"
-  for template_file in ${ZDOTDIR:-${HOME}}/.zim/templates/*; do
-    user_file="${ZDOTDIR:-${HOME}}/.${template_file:t}"
-    cat ${template_file} ${user_file}(.N) > ${user_file}.tmp && mv ${user_file}{.tmp,}
-  done
-
-  logInfo "Finishing optimization"
-  $(source ${ZDOTDIR:-${HOME}}/.zlogin)
+  curl -fsSL https://raw.githubusercontent.com/zimfw/install/master/install.zsh | zsh
 }
 
 if [ -d ${ZDOTDIR:-${HOME}}/.zim ]; then
@@ -27,7 +16,3 @@ if [ -d ${ZDOTDIR:-${HOME}}/.zim ]; then
 fi
 
 install_zim
-configure_zim
-
-logInfo "You can now update zim with: 'zmanage update'"
-logInfo "You can remove zim with: 'zmanage remove' (although this command is experimental)"
