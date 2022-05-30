8e89d38e7ed72a0634dd1a32a09ea052d02fd0ec
Gautham Chandra <gautham.chandra@live.com>
2022-05-30 09:36:24 -0400

    Update dotfiles to fix config for mobile development

--- zsh/config/android.zsh
+++ zsh/config/android.zsh
@@ -2,8 +2,7 @@ alias adpaste="adb -e shell input text"
 
 # Define the sdk home var used by some command line tools 
 # and add the execs like monkey and sdkmanager to path 
-export ANDROID_HOME=/usr/local/share/android-sdk
-export ANDROID_SDK_HOME=$ANDROID_HOME
+export ANDROID_HOME=$HOME/Library/Android/sdk
 export ANDROID_AVD_HOME=$HOME/.android/avd/
 export PATH=$PATH:$ANDROID_HOME/tools/bin
 export PATH=$PATH:$ANDROID_HOME/platform-tools
